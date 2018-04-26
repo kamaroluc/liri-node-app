7b9153d16255172f73cd4b7e0e472a57fe015a14
kamaroluc <kamaroluc@gmail.com>
2018-04-26 09:20:53 -0400

    spotify completed

--- liris2.js
+++ liris2.js
@@ -1,7 +1,7 @@
 
 require('dotenv').config()
 
-// requ
+// require some requirements
 var Twitter = require('twitter');
 var Spotify = require("node-spotify-api" );
 var keys= require("./keys.js");
@@ -9,29 +9,12 @@ var request = require("request");
 var twitterData = require("./keys.js");
 var fs = require("fs");
 var Twit = require('twit')
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+var client = new Twitter(keys.twitter)
 
-// var spotify = new Spotify(keys.spotify);
-
-
-
-//empty array to grad data
-// var mytweet = "";
-//twitter
 //***********twitter********
-// action = new Twitter({
-//         consumer_key: twitterData.consumer_key,
-//         consumer_secret: twitterData.consumer_secret,
-//         access_token_key: twitterData.access_token_key,
-//         access_token_secret: twitterData.access_token_secret
-//     });
-
-    // Build a string with my tweet.
-    // mytweet = mytweet + " " + action[i];
-    // console.log(mytweet)//  
-    
-  function mytweet(){
+//get into my acoounts using my keys
+//get the datas()
+function mytweet(){
     var client = new Twitter(keys.twitter);
     var params = {
         screen_name: "only_tell_truth"
@@ -47,12 +30,20 @@ var client = new Twitter(keys.twitter);
         }
     });
   }
-        //  console.log("Tweets" +tweets);
-//variable to grab action to be perform
-var action = process.argv;
-if(action[2]=== "showtweets"){
+//variables to grab action to be perform
+var action = process.argv[2];
+if(action === "showtweets") {
     mytweet();
+} 
+if (action === "showsongs") {
+  myMusic();
+  tweeterBot();
+  console.log('user type show song');
 }
+if(action === "do-what-it-says"){
+  doSays();
+}
+
    // If the request is successful (i.e. if the response status code is 200)
   
    
@@ -61,18 +52,49 @@ if(action[2]=== "showtweets"){
 //      console.log("I t: " + (JSON.stringify(tweets)));
 //    }
 //    });
-//           fs.writeFile("twitter-bot", tweets, function(err) {
+         function tweeterBot (){
+          fs.writeFile("twitter-bot", tweets, function(err) {
    
-//              // If the code experiences any errors it will log the error to the console.
-//              if (err) {
-//                return console.log(err);
-//              }
+             // If the code experiences any errors it will log the error to the console.
+             if (err) {
+               return console.log(err);
+             }
            
-//              // Otherwise, it will print: "movies.txt was updated!"
-//              console.log("");
-           
-//            });   
-// }
+             // Otherwise, it will print: "movies.txt was updated!"
+             console.log("");
+            
+           });   
+
+ }
+
+//**********spotify**********
+function myMusic(){
+var spotify = new Spotify(keys.spotify);
+
+var song = process.argv[3];
+console.log(song);
+spotify
+  .search({ type: 'track', query: song })
+  .then(function(response) {
+    console.log(response.tracks.items[0]);
+    console.log("Song Name: "+ response.tracks.items[0].name);
+    console.log("Album name: "+response.tracks.items[0].album.name);
+    console.log("Artist Name: "+response.tracks.items[0].album.artists[0].name);
+    console.log("Release Date: "+response.tracks.items[0].album.release_date);
+   
+  //   for (var i = 0; i < response.length; i++) {
+  //     console.log(response[i].created_at);+
+  //     console.log("");
+  //     console.log(response[i].text);
+  // console.log(response); 
+  //   }
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
+  }
+
+
 //***********movie********
 request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy", function(error, response, body) {
 
@@ -83,4 +105,30 @@ request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=tril
       console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
     }
   });
-  
+
+
+  //*********************** Do what it says */
+  function doSays(){
+    fs.readFile("random.text","utf8",function(err,data){
+      if(err){
+        return console.log(err);
+      }
+      var actionArr = data.split(',');
+      var spotify = new Spotify(keys.spotify);
+
+      var song = actionArr[1];
+console.log(song);
+spotify
+  .search({ type: 'track', query: song })
+  .then(function(response) {
+   // console.log(response.tracks.items[0]);
+    console.log("Song Name: "+ response.tracks.items[0].name);
+    console.log("Album name: "+response.tracks.items[0].album.name);
+    console.log("Artist Name: "+response.tracks.items[0].album.artists[0].name);
+    console.log("Release Date: "+response.tracks.items[0].album.release_date);
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
+    });
+  }
